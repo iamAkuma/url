74c088aea0c17aafc554ed060a06d6c496e92f1a
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-06 18:58:33 +0545

    adhuro

--- app.js
+++ app.js
@@ -10,10 +10,23 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+//Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017', {
+    dbName: 'url-shortener',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=> console.log('mongoose connected'))
+.catch(error => console.log('Error occurred...') )
+
 app.set('view engine', 'ejs');
 
 app.get('/', async(req, res, next)=> {
     res.render('index')
+})
+
+app.post('/', async(req, res, next) => {
+
 })
 
 app.listen(3000, () => console.log('Server on port 3000'))
